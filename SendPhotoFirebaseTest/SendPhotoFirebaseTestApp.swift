71978090b9222176c198eb99c09999e49818baf2
yukiny0811 <yukiny0811@gmail.com>
2024-06-19 18:53:23 +0900

    Initial Commit

---
+++ SendPhotoFirebaseTest/SendPhotoFirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendPhotoFirebaseTestApp.swift
+//  SendPhotoFirebaseTest
+//
+//  Created by Yuki Kuwashima on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct SendPhotoFirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
